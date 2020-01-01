35d0584db9837dfd1c707e9e1fc8dbb87061b9a5
Maxim <maxim@supermaxim.com>
2020-01-01 22:45:13 +0000

    added package uploader and restructured project

---
+++ Demo/Scripts/ChatSample/ChatMsgCountController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SuperMaxim.Messaging;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatMsgCountController : MonoBehaviour
+{
+    [SerializeField]
+    private Text _counterText;
+
+    private int _msgCount;
+
+    private void Awake()
+    {
+        Debug.AssertFormat(_counterText != null, "_counterText is not assigned");
+    }
+
+    private void Start()
+    {
+        Messenger.Default.Subscribe<ChatPayload>(OnChatMessageReceived);
+    }
+
+    private void OnDestroy()
+    {
+        Debug.LogFormat("{0} destoyed", this);
+    }
+
+    private void OnChatMessageReceived(ChatPayload payload)
+    {
+        Debug.LogFormat("Received: {0}", payload);
+
+        if(_counterText == null)
+        {
+            return;
+        }
+
+        _counterText.text = string.Format("Message Count: {0}", ++_msgCount);        
+    }
+
+    public void KillMe()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
---
+++ Demo/Scripts/ChatSample/ChatPayload.cs
@@ -0,0 +1,11 @@
+﻿public class ChatPayload
+{
+    public string UserId { get; set; }
+
+    public string Text { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("UserId: {0}, Text: {1}", UserId, Text);
+    }
+}
